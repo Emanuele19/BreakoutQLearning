5a523bbaf60962a59d829f6784c7cbc1abd24e95
Emanuele19 <emanuele190403@gmail.com>
2024-09-02 14:39:01 +0200

    Structure

---
+++ configs.py
@@ -0,0 +1,8 @@
+WIDTH, HEIGHT = 800, 600
+TRAIN_FPS = 480
+PLAY_FPS = 60
+SAMPLING_RATE = 20  # per la discretizzazione per Q-Learning
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
