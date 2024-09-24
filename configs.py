373ebdf5335c68381ec10bbfeeff4c6a3d993e1c
Emanuele19 <emanuele190403@gmail.com>
2024-09-24 20:38:22 +0200

    Introdotti meta parametri

--- configs.py
+++ configs.py
@@ -1,8 +1,16 @@
+# Program settings
 WIDTH, HEIGHT = 800, 600
 TRAIN_FPS = 480
 PLAY_FPS = 60
 SAMPLING_RATE = 30  # to make contiguous values discrete
 
+# Some useful constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+
+# model meta-parameters
+EPSILON = 0.9
+MIN_EPSILON = 0.1
+LEARNING_RATE = 0.001
+DISCOUNT_FACTOR = 0.999
